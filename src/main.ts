3befc48e406f803aade4d5608690f9870e3c925f
Tobias Kavsek <tobiaskavsek@hotmail.de>
2021-12-13 00:54:33 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
-﻿
+﻿export * from './model/character/character.interface';
+
+export * from './model/story/area.interface';
+export * from './model/story/asset.interface';
+export * from './model/story/effect.interface';
+export * from './model/story/place.interface';
+export * from './model/story/story.interface';
