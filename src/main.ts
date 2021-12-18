f8c66d10aa16a70ccc73e035aa3b21096b5b5e8b
CodingVampyre <tobiaskavsek@hotmail.de>
2021-12-18 22:31:47 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 ﻿export * from './model/character/character.interface';
 
-export * from './model/story/area.interface';
 export * from './model/story/asset.interface';
 export * from './model/story/effect.interface';
 export * from './model/story/place.interface';
 export * from './model/story/story.interface';
+
+export * from './controller/character.controller';
+export * from './controller/game.controller';
