ddb86eafbd5c1e154d2175e4678db46f97c9f232
Tobias Kavsek <tobiaskavsek@hotmail.de>
2021-12-19 19:45:09 +0100

    docs

--- src/controller/types/submit-decision-result.interface.ts
+++ src/controller/types/submit-decision-result.interface.ts
@@ -1,5 +1,6 @@
 ﻿import { IAssetInstance } from '../../model/story/asset.interface';
 
+/** metadata on how a decision ended, used to determine how a character should be updated */
 export interface SubmitDecisionResult {
 	characterGainsAssetInstances: IAssetInstance[];
 	characterLoosesAssetInstances: IAssetInstance[];
