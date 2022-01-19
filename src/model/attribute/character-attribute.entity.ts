2f6a8a194ea9a61818a1f029afee48d2864a2c51
Tobias Kavsek <tovuko@hotmail.de>
2022-01-19 14:39:56 +0100

    fix tests

--- src/model/attribute/character-attribute.entity.ts
+++ src/model/attribute/character-attribute.entity.ts
@@ -1,8 +1,7 @@
-import { Entity } from '../entity.interface';
 import { Attribute } from './attribute.entity';
 
 /** a character has multiple attributes and points in them */
-export interface CharacterAttribute extends Entity {
+export interface CharacterAttribute {
 	/** an attribute of a static lists */
 	attribute: Attribute;
 	/** used to calculate the level */
